ea13e8beb7965bad366771bc8282f37c57758161
mebr0 <aza17e@gmail.com>
2019-03-03 17:33:59 +0600

    __pycache__ added to .gitignore, added TOKEN constant, added /start method

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import telebot
 
-def main():
-	pass
+from constants import *
 
+bot = telebot.TeleBot(TOKEN)				# Bot creating through TOKEN
+
+# /start
+@bot.message_handler(commands=['start'])
+def start(message):
+	bot.send_message(message.chat.id, "Hello")
+
+def main():									# method for bot polling
+	print('Started!')
+
+	bot.polling()
 
 if __name__ == '__main__':
 	main()
